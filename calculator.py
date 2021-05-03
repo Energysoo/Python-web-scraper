cb7fe07a76e65c9ee17f5f5786f63d0fd95322f7
Energysoo <jsmail819@naver.com>
2021-05-03 23:52:38 +0900

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def plus(a, b):
+    return a+b
+
+
+def minus(a, b):
+    return a-b
+
+
+def multiple(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
+
+
+def downgrade(a, b):
+    return a//b
+
+
+def left(a, b):
+    return a % b
+
+
+calculator_plus = plus(3, 5)
+calculator_minus = minus(3, 5)
+calculator_multiple = multiple(3, 5)
+calculator_devide = devide(3, 5)
+calculator_downgrade = downgrade(3, 5)
+calculator_left = left(3, 5)
+print(int(calculator_plus))
+print(int(calculator_minus))
+print(int(calculator_multiple))
+print(int(calculator_divde)
+print(int(calculator_downgrade))
+print(int(calculator_left))
