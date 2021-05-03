c59f95cca28fb917e323efbfdacc39c5af6d0b82
Energysoo <jsmail819@naver.com>
2021-05-04 00:02:46 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,36 +1,30 @@
 def plus(a, b):
-    return a+b
+    return (int(a)+int(b))
 
 
 def minus(a, b):
-    return a-b
+    return (int(a)-int(b))
 
 
 def multiple(a, b):
-    return a*b
+    return (int(a)*int(b))
 
 
 def divide(a, b):
-    return a/b
+    return (int(a)/int(b))
 
 
 def downgrade(a, b):
-    return a//b
+    return (int(a)//int(b))
 
 
 def left(a, b):
-    return a % b
+    return (int(a) % int(b))
 
 
-calculator_plus = plus(3, 5)
-calculator_minus = minus(3, 5)
-calculator_multiple = multiple(3, 5)
-calculator_devide = devide(3, 5)
-calculator_downgrade = downgrade(3, 5)
-calculator_left = left(3, 5)
-print(int(calculator_plus))
-print(int(calculator_minus))
-print(int(calculator_multiple))
-print(int(calculator_divde)
-print(int(calculator_downgrade))
-print(int(calculator_left))
+print(plus("20", 5))
+print(minus("20", 5))
+print(multiple("20", 5))
+print(divide("20", 5))
+print(downgrade("20", 5))
+print(left("20", 5))
